dfa835e6712d776cd7d4024eaa45b2db987b9c67
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-04-12 12:30:58 +0530

    1.30

---
+++ backend/config.py
@@ -0,0 +1,21 @@
+from decouple import config
+import os
+
+# Reading configuration variables from .env file
+BAS_DIR=os.path.dirname(os.path.realpath(__file__))
+class Config:
+    SECRET_KEY=config('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI="sqlite:///"+os.path.join(BAS_DIR,'dev.db')
+    DEBUG=True
+    SqLALCHEMY_ECHO=True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+    
---
+++ help.md
@@ -0,0 +1,15 @@
+# flask
+py -3 -m venv venv -to create virutual env
+
+ venv\scripts\activate - to activate
+
+$env:FLASK_APP = "main.py"
+
+for setting environment variable
+
+python main.py -to run api
+
+
+echo $env:FLASK_APP
+
+
